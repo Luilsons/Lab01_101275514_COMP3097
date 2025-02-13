90b84a97999591274fdb285e3e97fe043ea38a2f
Luilson Sousa <luilsonms@hotmail.com>
2025-02-13 18:28:57 -0500

    Initial Commit

---
+++ Lab01_Luilson_Sousa/Lab01_Luilson_SousaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab01_Luilson_SousaApp.swift
+//  Lab01_Luilson_Sousa
+//
+//  Created by Luilson Marcos Sousa dos Reis on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct Lab01_Luilson_SousaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
